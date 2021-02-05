35dd168f1aaf8f1bfda8182c3025967b9767f6e0
Gil Solis <gilsolisuw@gmail.com>
2021-02-04 19:43:15 -0800

    updated url and response

--- js/main.js
+++ js/main.js
@@ -4,21 +4,14 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch() {
     const choice = document.querySelector('input').value.split(' ').join('+')
     console.log(choice)
-    const url = 'http://api.giphy.com/v1/gifs/search?q=' + choice + '&api_key=cDHLAYdG5rExJkjmVa1Is0m59bOZTsk8'
-    const url2 = "https://api.tenor.com/v1/search?q=excited&key=LIVDSRZULELA&limit=8"
-
+    const url = "https://api.giphy.com/v1/gifs/search?api_key=cDHLAYdG5rExJkjmVa1Is0m59bOZTsk8&limit=1&rating=pg13&q=" + choice
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
             console.log(data)
             console.log(data.data[0].bitly_url)
-            // console.log(data.data[0].url)
-            // document.querySelector('link').href = data.data[0].url
-            // document.querySelector('iframe').src = data.data[0].url + '/giphy.gif'
-            // document.querySelector('iframe').src = data.data[0].bitly_url
-            // document.querySelector('iframe').style.height = "80vh"
             document.querySelector('img').style.visibility = 'visible'
-            document.querySelector('img').src = 'https://media.giphy.com/media/' + data.data[0].id + '/giphy.gif'
+            document.querySelector('img').src = data.data[0].images.original.url
 
         })
         .catch(err => {
